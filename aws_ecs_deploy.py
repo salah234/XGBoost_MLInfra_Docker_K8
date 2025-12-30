ebaa1330ec95aef165a0e659c0d2c9970b0e12fb
Salah Mohamed <salahm2130@Salahs-MacBook-Pro-7.local>
2025-12-30 17:42:19 -0600

    Finished ECR starting on ECS and AWS Fargate

--- aws_ecs_deploy.py
+++ aws_ecs_deploy.py
@@ -1,14 +1,40 @@
-import boto3 
+import boto3  # AWS SDK for Python
 import os 
 from dotenv import load_dotenv
+import base64
+
+# Using ECS on AWS Fargate to run inference and training 
+# Workflow:
+    # Amazon ECR to create repositories to store the docker images in.
+    # Amazon ECS to define how to run the containers
+    # AWS Fargate to run the containers in a serverless manner
+    # Need Auth Token for ECR
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION")
+AWS_REGION = os.getenv("AWS_DEFAULT_REGION")
 
 
-ecs = boto3.client('ecs', region_name=AWS_REGION)
+
+ecr = boto3.client('ecr', region_name=AWS_REGION)
+repo_stages = ['xgb-train', 'inference'] # Match it with docker image tages
+for repo in repo_stages:
+    try:
+        ecr.create_repository(repositoryName=repo)
+        print(f"Created ECR Repository for {repo}")
+    except ecr.exceptions.RepositoryAlreadyExistsException:
+          print(f"Repository {repo} already exists")
+
+    
+
+auth = ecr.get_authorization_token()
+token = auth['authorizationData'][0]['authorizationToken']
+user, password = base64.b64decode(token).decode().split(':')
+registry = auth['authorizationData'][0]['proxyEndpoint']
+
+
+
 
 
